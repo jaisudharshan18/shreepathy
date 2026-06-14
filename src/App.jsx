fdb92ff3da4b445de9fa551a6de74ab5dd5b18d4
jaisudharshan18 <jaisudharshanjaisudharshan.com>
2026-06-14 17:16:59 +0530

    Fix: use HashRouter and add 404.html for GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 import MarketingLayout from "./pages/MarketingLayout";
 import ShopLayout from "./pages/ShopLayout";
 import AccountLayout from "./pages/AccountLayout";
